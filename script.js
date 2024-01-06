e86341266c538d079fc9609ff9e42d6a1585268e
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-01-06 09:38:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                lat:45.227089,
-                lng:5.822510,
+                lat:35.6186768,
+                lng:5.2753539,
             },
         },
     ];
